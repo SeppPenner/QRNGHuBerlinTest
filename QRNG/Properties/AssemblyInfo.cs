9336b681a563f10df5ae126af29412f73fab972c
SeppPenner <HansM2013@gmx.de>
2019-10-27 20:56:41 +0100

    Updated nuget packages, added GitVersionTask.

--- QRNG/Properties/AssemblyInfo.cs
+++ QRNG/Properties/AssemblyInfo.cs
@@ -13,6 +13,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("7391df1d-7d51-4d72-a825-684f3eb303c3")]
-
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
